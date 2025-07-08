10c7dba4f143d2eb64c2ab6b1f5f3dc61168af1d
ShinyaYoshida-biomet <slmnphmet1.2@gmail.com>
2025-07-09 05:35:47 +0900

    add test

--- test/todo_service_test.dart
+++ test/todo_service_test.dart
@@ -1,46 +1,9 @@
-
-import 'package:flutter/foundation.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:tsubusu/models/todo.dart';
-import 'package:tsubusu/services/shared_todo_service.dart';
 import 'package:tsubusu/services/window_todo_service.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
-
-  group('SharedTodoService', () {
-    late SharedTodoService sharedTodoService;
-
-    setUp(() async {
-      SharedPreferences.setMockInitialValues({});
-      SharedTodoService.reset();
-      sharedTodoService = SharedTodoService.instance;
-      await sharedTodoService.addTodo('Todo 1');
-      await sharedTodoService.addTodo('Todo 2');
-      await sharedTodoService.addTodo('Todo 3');
-    });
-
-    test('reorderTodo should correctly reorder todos when moving down', () async {
-      // Act
-      await sharedTodoService.reorderTodo(0, 3);
-
-      // Assert
-      expect(sharedTodoService.todos[0].text, 'Todo 2');
-      expect(sharedTodoService.todos[1].text, 'Todo 3');
-      expect(sharedTodoService.todos[2].text, 'Todo 1');
-    });
-
-    test('reorderTodo should correctly reorder todos when moving up', () async {
-      // Act
-      await sharedTodoService.reorderTodo(2, 0);
-
-      // Assert
-      expect(sharedTodoService.todos[0].text, 'Todo 3');
-      expect(sharedTodoService.todos[1].text, 'Todo 1');
-      expect(sharedTodoService.todos[2].text, 'Todo 2');
-    });
-  });
 
   group('WindowTodoService', () {
     late WindowTodoService windowTodoService;
