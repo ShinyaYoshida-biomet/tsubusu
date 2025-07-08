9025c2d829d430b0f47aaa0b1f66cfb831f7ccc3
ShinyaYoshida-biomet <slmnphmet1.2@gmail.com>
2025-07-09 05:46:28 +0900

    refactoring to remove unnecessary platform_channel service

--- lib/main.dart
+++ lib/main.dart
@@ -1,59 +1,25 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:desktop_multi_window/desktop_multi_window.dart';
 import 'providers/theme_provider.dart';
-import 'services/platform_channel_handler.dart';
+import 'services/window_manager.dart';
 import 'widgets/pages/todo_page.dart';
 
 void main(List<String> args) {
   WidgetsFlutterBinding.ensureInitialized();
-  
+
   if (args.firstOrNull == 'multi_window') {
-    // This is a sub window
     final windowId = int.parse(args[1]);
-    final argument = args[2];
-    runApp(TsubusuSubWindow(
-      windowController: WindowController.fromWindowId(windowId),
-      args: argument,
-    ));
+    runApp(TsubusuWindow(windowController: WindowController.fromWindowId(windowId)));
   } else {
-    // This is the main window
-    PlatformChannelHandler.initialize();
-    runApp(const TsubusuApp());
+    runApp(const TsubusuWindow());
   }
 }
 
-class TsubusuApp extends StatelessWidget {
-  const TsubusuApp({super.key});
+class TsubusuWindow extends StatelessWidget {
+  final WindowController? windowController;
 
-  @override
-  Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => ThemeProvider(),
-      child: Consumer<ThemeProvider>(
-        builder: (context, themeProvider, child) {
-          return MaterialApp(
-            title: 'Tsubusu',
-            theme: themeProvider.themeData,
-            home: const TodoPage(),
-            debugShowCheckedModeBanner: false,
-          );
-        },
-      ),
-    );
-  }
-}
-
-class TsubusuSubWindow extends StatelessWidget {
-  final WindowController windowController;
-  final String args;
-
-  const TsubusuSubWindow({
-    super.key,
-    required this.windowController,
-    required this.args,
-  });
+  const TsubusuWindow({super.key, this.windowController});
 
   @override
   Widget build(BuildContext context) {
